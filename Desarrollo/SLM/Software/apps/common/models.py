6d271bcf69cce9990ab1f7ebc451de1ca86fcbca
Junior Zavaleta <juniorclaudiozavaleta@gmail.com>
2017-10-25 14:52:26 -0500

    Modelos y migraciones iniciales - SLM

---
+++ Desarrollo/SLM/Software/apps/common/models.py
@@ -0,0 +1,90 @@
+from django.db import models
+from django.utils import timezone
+from django.db.models import QuerySet
+
+
+class SoftDeletionQuerySet(QuerySet):
+    def delete(self):
+        return super(SoftDeletionQuerySet, self).update(deleted_at=timezone.now())
+
+    def hard_delete(self):
+        return super(SoftDeletionQuerySet, self).delete()
+
+    def alive(self):
+        return self.filter(deleted_at=None)
+
+    def dead(self):
+        return self.exclude(deleted_at=None)
+
+
+class SoftDeletionManager(models.Manager):
+    def __init__(self, *args, **kwargs):
+        self.alive_only = kwargs.pop('alive_only', True)
+        super(SoftDeletionManager, self).__init__(*args, **kwargs)
+
+    def get_queryset(self):
+        if self.alive_only:
+            return SoftDeletionQuerySet(self.model).filter(deleted_at=None)
+        return SoftDeletionQuerySet(self.model)
+
+    def hard_delete(self):
+        return self.get_queryset().hard_delete()
+
+
+class SoftDeletionModel(models.Model):
+    deleted_at = models.DateTimeField(blank=True, null=True)
+
+    objects = SoftDeletionManager()
+    all_objects = SoftDeletionManager(alive_only=False)
+
+    class Meta:
+        abstract = True
+
+    def delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    def hard_delete(self):
+        super(SoftDeletionModel, self).delete()
+
+
+class BaseModel(SoftDeletionModel):
+    created_at = models.DateTimeField(blank=True, null=True, editable=False, auto_now_add=True)
+    modified_at = models.DateTimeField(blank=True, null=True, editable=False, auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.pk:
+            self.modified_at = timezone.now()
+        else:
+            self.created_at = timezone.now()
+            kwargs['force_insert'] = False
+        super().save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
+
+
+class Departamento(BaseModel):
+    nombre = models.CharField(max_length=40)
+
+    class Meta:
+        verbose_name = 'Departamento'
+        verbose_name_plural = 'Departamentos'
+
+
+class Provincia(BaseModel):
+    nombre = models.CharField(max_length=40)
+    provincia = models.ForeignKey(Departamento)
+
+    class Meta:
+        verbose_name = 'Provincia'
+        verbose_name_plural = 'Provincias'
+
+
+class Distrito(BaseModel):
+    nombre = models.CharField(max_length=40)
+    provincia = models.ForeignKey(Provincia)
+
+    class Meta:
+        verbose_name = 'Distrito'
+        verbose_name_plural = 'Distritos'
--- Desarrollo/SLM/Software/apps/slm/admin.py
+++ Desarrollo/SLM/Software/apps/slm/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TipoDocumento, TipoInstrumento, Instrumento, Local, Cliente, GeneroMusical
+
+
+admin.site.register(TipoDocumento)
+admin.site.register(TipoInstrumento)
+admin.site.register(Instrumento)
+admin.site.register(Local)
+admin.site.register(Cliente)
+admin.site.register(GeneroMusical)
