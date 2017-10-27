85e1a8c63a3cb9291d9cdc20ebfc05cbf6c6d168
staffcreativa <chavezvasquezjuan@gmail.com>
2017-10-27 14:56:10 -0500

    correciones en admin de departamento distrito provincia

--- Desarrollo/SLM/Software/apps/common/admin.py
+++ Desarrollo/SLM/Software/apps/common/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Distrito, Provincia, Departamento
 
 
-admin.site.register(Departamento)
-admin.site.register(Provincia)
-admin.site.register(Distrito)
+@admin.register(Departamento)
+class DepartamentoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Provincia)
+class ServiciosAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Distrito)
+class DistritoAdmin(admin.ModelAdmin):
+    pass
