376e20b8b1c181931477531ddaa566933cfff534
staffcreativa <chavezvasquezjuan@gmail.com>
2017-10-27 14:58:20 -0500

    correciones en los modelos de SLM

--- Desarrollo/SLM/Software/apps/slm/admin.py
+++ Desarrollo/SLM/Software/apps/slm/admin.py
@@ -3,9 +3,31 @@ from django.contrib import admin
 from .models import TipoDocumento, TipoInstrumento, Instrumento, Local, Cliente, GeneroMusical
 
 
-admin.site.register(TipoDocumento)
-admin.site.register(TipoInstrumento)
-admin.site.register(Instrumento)
-admin.site.register(Local)
-admin.site.register(Cliente)
-admin.site.register(GeneroMusical)
+@admin.register(TipoDocumento)
+class TipoDocumentoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TipoInstrumento)
+class TipoInstrumentoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Instrumento)
+class InstrumentoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Local)
+class LocalAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GeneroMusical)
+class GeneroMusicalAdmin(admin.ModelAdmin):
+    pass
